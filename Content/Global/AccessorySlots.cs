f85472815c73687b961ff94a29606c443679faa9
David Alvarado Rodríguez <davidalvaradorguez@gmail.com>
2025-03-03 15:30:25 +0100

    Alternate click and start developing Weapon arts.

---
+++ Content/Global/AccessorySlots.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.ModLoader.IO;
+using FireEmblem3H.Content.Global;
+
+namespace FireEmblem3H.Content.Global
+{
+    public class SwordArtSlot : ModAccessorySlot
+    {
+        public override bool CanAcceptItem(Item item, AccessorySlotType context)
+        {
+            if (item.wingSlot > 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public override bool DrawVanitySlot => false;
+        public override bool DrawDyeSlot => false;
+    }
+}
--- Content/Items/Weapons/Thunderbrand.cs
+++ Content/Items/Weapons/Thunderbrand.cs
@@ -5,11 +5,24 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.IO;
+using Terraria.GameContent;
+using Terraria.Localization;
+using Terraria.ModLoader.IO;
 
 namespace FireEmblem3H.Content.Items.Weapons
 {
     public class Thunderbrand : ModItem
     {
+
+        public static readonly int MaxUses = 300;
+
+        public int useCount;
+
+        public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(MaxUses);
+
         public override void SetStaticDefaults()
         {
         }
@@ -29,11 +42,20 @@ namespace FireEmblem3H.Content.Items.Weapons
             Item.useAnimation = 15;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.UseSound = SoundID.Item1;
+
+            Item.consumable = true;
+
             Item.autoReuse = true;
             Item.useTurn = true;
+            ItemID.Sets.ItemsThatAllowRepeatedRightClick[Item.type] = true;
 
             Item.rare = ItemRarityID.Purple;
             Item.value = 5000000;
+        }
+
+        public override bool AltFunctionUse(Player player)
+        {
+            return true;
         }
 
         public override void ModifyWeaponDamage(Player player, ref StatModifier damage)
@@ -54,7 +76,96 @@ namespace FireEmblem3H.Content.Items.Weapons
 
         public override void UseAnimation(Player player)
         {
+            if (player.altFunctionUse == 2)
+            {
+                Item.useStyle = ItemUseStyleID.Swing;
+                Item.shootSpeed = 15f;
+                Item.damage = 400;
+                if (player.GetModPlayer<Jugador>().Charon)
+                {
+                    Item.shoot = ProjectileID.StarWrath;
+                    Item.ArmorPenetration = 10;
+                }
+                else
+                {
+                    Item.shoot = ProjectileID.Starfury;
+                }
+            }
+            else
+            {
+                Item.useStyle = ItemUseStyleID.Swing;
+                Item.shootSpeed = 10f;
+                Item.shoot = 0;
+                Item.damage = 200;
+            }
             player.GetModPlayer<Jugador>().checkCrest();
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            if (player.altFunctionUse == 2)
+            {
+                useCount += 3;
+            }
+
+            if (player.whoAmI != Main.myPlayer)
+            {
+                return true;
+            }
+
+            return true;
+        }
+
+        public override bool ConsumeItem(Player player)
+        {
+            useCount++;
+            if (useCount >= MaxUses)
+            {
+                useCount = 0;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void SaveData(TagCompound tag)
+        {
+            tag["useCount"] = useCount;
+        }
+
+        public override void LoadData(TagCompound tag)
+        {
+            useCount = tag.Get<int>("useCount");
+        }
+
+        public override void NetSend(BinaryWriter writer)
+        {
+            writer.Write(useCount);
+        }
+
+        public override void NetReceive(BinaryReader reader)
+        {
+            useCount = reader.ReadInt32();
+        }
+
+        public override void PostDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            if (useCount == 0)
+            {
+                return;
+            }
+
+            Vector2 spriteSize = frame.Size() * scale;
+
+            spriteBatch.Draw(TextureAssets.MagicPixel.Value,
+                position: new Vector2(position.X, position.Y + spriteSize.Y * 0.9f),
+                sourceRectangle: new Rectangle(0, 0, 1, 1),
+                Color.Red,
+                rotation: 0f,
+                origin: Vector2.Zero,
+                scale: new Vector2(spriteSize.X * (MaxUses - useCount) / MaxUses, 2f),
+                SpriteEffects.None,
+                layerDepth: 0f);
         }
 
         public override void AddRecipes()
